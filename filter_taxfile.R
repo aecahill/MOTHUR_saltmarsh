94d08884b85fcc53df92ffc8aa650b2b2c011cf9
aecahill <aecsk8gr8@yahoo.com>
2018-10-17 17:50:38 -0400

    updates_oct17
    Updating code to try to remove sequences from scrap.pcr file from mothur

--- filter_taxfile.R
+++ filter_taxfile.R
@@ -22,3 +22,46 @@ filtered<-tax[-toss,]
 dim(filtered)
 
 length(filtered$ï..V1)==length(tax$ï..V1)-length(toss)
+
+length(toss)==length(discard$V1)
+
+#need to read in scrap.pcr sequences using SeqinR and remove those numbers as well
+
+library(seqinr)
+
+#read in fasta file as alignment
+
+scrapalign<-read.alignment("C:/Users/acahill/Desktop/scrap.fasta.align",format="fasta")
+listnames<-as.list(scrapalign$nam)
+
+clean_names=list() #empty vector of names
+nameslength<-c(1:length(listnames)) #vector from 1 to 2014 (number of seqs)
+
+for (i in nameslength){
+  d<-gsub("[\t]", "", listnames[i])
+  clean_names<-c(clean_names,d)
+  
+}
+
+clean_names[1:5]
+length(clean_names)
+
+#now remove these names from the filtered file
+
+toss2<-c()
+
+for (i in clean_names){
+  rownumber2<-match(i,filtered$ï..V1)
+  toss2<-append(toss2,rownumber2)
+  
+}
+
+length(toss2)
+
+filtered2<-tax[-toss2,]
+
+dim(filtered2)
+
+
+
+write.table(filtered, file = "C:/Users/acahill/Desktop/filtered.tax",row.names=FALSE,sep= )
