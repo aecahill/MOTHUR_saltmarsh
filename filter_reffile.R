fd65e5e593f9ade683c94dc165e4ba6f28b7f0a8
aecahill <aecsk8gr8@yahoo.com>
2018-11-12 11:49:10 -0500

    changes_nov12
    Trying to revise code to filter these taxfiles so mothur works

--- filter_reffile.R
+++ filter_reffile.R
@@ -1,3 +1,6 @@
+library(seqinr)
+
+
 #read in reference alignment
 
 refalign<-read.alignment("C:/Users/acahill/Desktop/reference.align",format="fasta")
@@ -26,3 +29,76 @@ for (i in namesreflength){
 }
 
 clean_namesref[1:5]
+
+#match names in seqs_2remove to line numbers in the alignment
+
+tossref<-c()
+
+#make vector to toss FROM REFERENCE
+for (i in clean_namesref){
+  rownumberref<-match(i,seqs_2remove)
+  tossref<-append(tossref,rownumberref)
+  
+}
+
+length(tossref)
+
+tossref_noNA<-na.omit(tossref)
+
+reffiltered_names<-refalign$nam[-tossref_noNA]
+reffiltered_seqs<-refalign$seq[-tossref_noNA]
+
+write.fasta(sequences=reffiltered_seqs,names=reffiltered_names,file.out="C:/Users/acahill/Desktop/reffiltered.fasta")
+
+#read in reference alignment
+
+refalign<-read.alignment("C:/Users/acahill/Desktop/reference.align",format="fasta")
+
+#read in table of errors thrown by mothur 
+#these are seqs that are in the ref but missing from the tax file; need to be removed from ref
+
+errortab<-read.table("C:/Users/acahill/Desktop/error_list.txt",header=FALSE)
+
+#turn names in error table into characters
+
+seqs_2remove<-as.character(errortab$V1)
+
+#need to clean reference names again
+
+listnamesref<-as.list(refalign$nam) #make a list of names of sequences
+
+clean_namesref=list() #empty vector of names
+namesreflength<-c(1:length(listnamesref)) #vector from 1 to number of seqs
+
+#change the format so that the seq names match the format in the taxonomy file
+for (i in namesreflength){
+  e<-gsub("[\t]", "", listnamesref[i])
+  clean_namesref<-c(clean_namesref,e)
+  
+}
+
+clean_namesref[1:5]
+
+tossref<-c()
+
+#flagging sequences that are in the scrap file
+for (i in clean_namesref){
+  rownumberref<-match(i,seqs_2remove)
+  tossref<-append(tossref,rownumberref)
+  
+}
+
+length(tossref)
+
+tossref_noNA<-na.omit(tossref)
+
+
+namestoss<-clean_namesref[-tossref_noNA]
+
+
+seqstoss<-refalign$seq[-tossref_noNA]
+
+length(namestoss)==length(seqstoss)
+
+write.fasta(sequnces=seqstoss,names=namestoss,file.out="C:/Users/acahill/Desktop/reffilteredtry2.fasta")
+
--- filter_taxfile.R
+++ filter_taxfile.R
@@ -1,3 +1,122 @@
+
+library(seqinr)
+
+tax<-read.table("C:/Users/acahill/Desktop/BOLD_taxonomy.tax",header=T) #this file from JKP as reference taxonomy from BOLD
+head(tax)
+
+discard<-read.table("C:/Users/acahill/Desktop/discard.accnos",header=F) #seqs discarded from MOTHUR because they didn't align
+head(discard)
+
+dim(tax)
+dim(discard)
+
+toss<-c() #empty vector
+
+#identify discarded seqs in the taxonomy database and flag their row numbers as a vector
+for (i in discard$V1){
+  rownumber<-match(i,tax$ï..V1)
+  toss<-append(toss,rownumber)
+  
+}
+
+length(toss)
+
+filtered<-tax[-toss,] #remove rows that correspond to discarded seqs
+
+dim(filtered)
+
+length(filtered$ï..V1)==length(tax$ï..V1)-length(toss)
+
+length(toss)==length(discard$V1)
+
+#need to read in scrap.pcr sequences using SeqinR and remove those numbers as well
+#these are sequences that when pcr trimmed had no bases left from MOTHUR
+
+#read in fasta file as alignment
+scrapalign<-read.alignment("C:/Users/acahill/Desktop/scrap.fasta.align",format="fasta")
+listnames<-as.list(scrapalign$nam) #make a list of names of sequences
+
+clean_names=list() #empty vector of names
+nameslength<-c(1:length(listnames)) #vector from 1 to 2014 (number of seqs)
+
+#change the format so that the seq names match the format in the taxonomy file
+for (i in nameslength){
+  d<-gsub("[\t]", "", listnames[i])
+  clean_names<-c(clean_names,d)
+  
+}
+
+clean_names[1:5]
+length(clean_names)
+
+#now remove these names from the filtered taxonomy file
+
+toss2<-c()
+
+#flagging sequences that are in the scrap file
+for (i in clean_names){
+  rownumber2<-match(i,filtered$ï..V1)
+  toss2<-append(toss2,rownumber2)
+  
+}
+
+length(toss2)
+
+#remove NAs from list (they are here because some of these are already tossed, I think)
+toss2_noNA<-na.omit(toss2)
+
+#remove scrap sequences to create a new filtered taxonomy
+filtered2<-filtered[-toss2_noNA,]
+
+dim(filtered2)
+
+#MOVING THIS TO A SEPARATE CODE WINDOW
+#now need to remove stuff in the reference that is not in the taxonomy.
+#read in large alignment file with seqinR -- may need to jigger names again?
+
+refalign<-read.alignment("C:/Users/acahill/Desktop/reference.align",format="fasta")
+
+#clean up names again
+
+listnamesref<-as.list(refalign$nam) #make a list of names of sequences
+
+clean_namesref=list() #empty vector of names
+namesreflength<-c(1:length(listnamesref)) #vector from 1 to number of seqs
+
+#change the format so that the seq names match the format in the taxonomy file
+for (i in namesreflength){
+  e<-gsub("[\t]", "", listnamesref[i])
+  clean_namesref<-c(clean_namesref,e)
+  
+}
+
+clean_namesref[1:5]
+length(clean_namesref)
+
+
+#use matching to check reference against filtered taxonomy file (filtered2)
+
+matches<-c()
+
+for (i in clean_namesref){
+  matchq<-match(i,filtered2$ï..V1)
+  matches<-append(matches,matchq)
+  
+}
+
+length(matches)
+head(matches)
+
+#if is.na(matches[i])=TRUE, remove 
+
+
+#if match found, T, else, F to create vector of T and F
+#remove lines corresponding to F rows
+#write out new alignment
+
+
+#write out taxonomy file
+
 library(seqinr)
 
 tax<-read.table("C:/Users/acahill/Desktop/BOLD_taxonomy.tax",header=T) #this file from JKP as reference taxonomy from BOLD
