7a27af16e6b2dd1f5848e4370ecc3a8f1a58aab1
aecahill <aecsk8gr8@yahoo.com>
2018-11-14 16:45:10 -0500

    nov_14_tryagainfilter
    Trying again to take out erroneous seqs from the alignment

--- filter_ref_try2.R
+++ filter_ref_try2.R
@@ -8,11 +8,11 @@ refalign<-read.alignment("C:/Users/acahill/Desktop/reference.align",format="fast
 #read in table of errors thrown by mothur 
 #these are seqs that are in the ref but missing from the tax file; need to be removed from ref
 
-errortab<-read.table("C:/Users/acahill/Desktop/error_list.txt",header=FALSE)
+#errortab<-read.table("C:/Users/acahill/Desktop/error_list.txt",header=FALSE)
 
 #turn names in error table into characters
 
-seqs_2remove<-as.character(errortab$V1)
+#seqs_2remove<-as.character(errortab$V1)
 
 #need to clean reference names again
 
@@ -29,14 +29,15 @@ for (i in namesreflength){
 }
 
 clean_namesref[1:5]
+length(clean_namesref)
 
-#match names in seqs_2remove to line numbers in the alignment
+#match names in filtered2 to line numbers in the alignment
 
 tossref<-c()
 
 #make vector to toss FROM REFERENCE
 for (i in clean_namesref){
-  rownumberref<-match(i,seqs_2remove)
+  rownumberref<-match(i,filtered2$ï..V1)
   tossref<-append(tossref,rownumberref)
   
 }
@@ -49,5 +50,4 @@ reffiltered_names<-refalign$nam[-tossref_noNA]
 reffiltered_seqs<-refalign$seq[-tossref_noNA]
 
 write.fasta(sequences=reffiltered_seqs,names=reffiltered_names,file.out="C:/Users/acahill/Desktop/reffiltered.fasta")
-
 
